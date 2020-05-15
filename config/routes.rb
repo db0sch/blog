a97391316e36f79c24725d691da4024a04887374
Dimitri Bosch <dimitribosch@hotmail.com>
2020-05-15 09:32:20 +0200

    Change routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 
 Rails.application.routes.draw do
-  root to: 'articles#index'
-  resources :articles
+  root to: 'articles#index' # => '/'
+  resources :articles, except: :index
 end
 
 # /articles INDEX
